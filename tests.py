8b595f4a2b89d264431e2d67e463a4e86cf30af6
Birkin James Diana <birkin.diana@gmail.com>
2024-12-26 07:36:36 -0500

    implemements test (need to code to pass).

--- tests.py
+++ tests.py
@@ -4,11 +4,27 @@
 # ///
 
 import unittest
+from pathlib import Path
+
+import self_updater
 
 
 class TestSelfUpdater(unittest.TestCase):
-    def test_update(self):
-        self.fail('Not implemented')
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test__compare_with_previous_backup__no_differences_A(self):
+        """
+        Files A and B differ only in date in comment-line, so should be considered equal.
+        """
+        file_a_new_path = Path('./test_docs/file_a.txt')
+        file_b_old_path = Path('./test_docs/file_b.txt')
+        expected = False
+        change_check_result = self_updater.compare_with_previous_backup(file_a_new_path, file_b_old_path)
+        self.assertEqual(expected, change_check_result)
 
 
 if __name__ == '__main__':
