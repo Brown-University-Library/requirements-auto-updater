f29bebebc564c1ca00aa56b78d232289b8071d15
Birkin James Diana <birkin.diana@gmail.com>
2024-12-26 08:06:43 -0500

    adds test-files.

--- tests.py
+++ tests.py
@@ -22,8 +22,20 @@ class TestSelfUpdater(unittest.TestCase):
         """
         file_a_new_path = Path('./test_docs/no_differences_A/file_a.txt').resolve()
         file_b_old_path = Path('./test_docs/no_differences_A/file_b.txt').resolve()
+        project_path = None
         expected = False
-        change_check_result = self_updater.compare_with_previous_backup(file_a_new_path, file_b_old_path)
+        change_check_result = self_updater.compare_with_previous_backup(file_a_new_path, file_b_old_path, project_path)
+        self.assertEqual(expected, change_check_result)
+
+    def test__compare_with_previous_backup__no_differences_B(self):
+        """
+        Files A and B differ in date in comment-line, A has "ACTIVE" in comment-line -- so should be considered equal.
+        """
+        file_a_new_path = Path('./test_docs/no_differences_B/file_a.txt').resolve()
+        file_b_old_path = Path('./test_docs/no_differences_B/file_b.txt').resolve()
+        project_path = None
+        expected = False
+        change_check_result = self_updater.compare_with_previous_backup(file_a_new_path, file_b_old_path, project_path)
         self.assertEqual(expected, change_check_result)
 
 
