9d2e8d10633325572ec68d129ec542a898f1f261
Birkin James Diana <birkin.diana@gmail.com>
2025-01-02 08:26:44 -0500

    implements refactor of run_tests.py call to _not_ assume any inline-metadata-script.

--- lib_call_runtests.py
+++ lib_call_runtests.py
@@ -9,33 +9,6 @@ from lib_emailer import Emailer
 log = logging.getLogger(__name__)
 
 
-# def run_initial_tests(uv_path: Path, project_path: Path, project_email_addresses: list[list[str, str]]) -> None:
-#     """
-#     Run initial tests to ensure that the script can run.
-
-#     On failure:
-#     - Emails project-admins
-#     - Raises an exception
-#     """
-#     log.debug('starting run_initial_tests()')
-#     run_tests_initial_path = project_path / 'run_tests.py'
-#     run_tests_path = run_tests_initial_path.resolve()
-#     try:
-#         command = [str(uv_path), 'run', str(run_tests_path)]
-#         log.debug(f'command: ``{command}``')
-#         subprocess.run(command, check=True)
-#     except Exception as e:
-#         message = f'Error on initial run_tests() call: ``{e}``. Halting self-update.'
-#         log.exception(message)
-#         ## email sys-admins -----------------------------------------
-#         emailer = Emailer(project_path)
-#         email_message: str = emailer.create_setup_problem_message(message)
-#         emailer.send_email(project_email_addresses, email_message)
-#         ## raise exception -----------------------------------------
-#         raise Exception(message)
-#     return
-
-
 def run_initial_tests(uv_path: Path, project_path: Path, project_email_addresses: list[list[str, str]]) -> None:
     """
     Run initial tests to ensure that the script can run.
@@ -85,11 +58,21 @@ def run_followup_tests(uv_path: Path, project_path: Path, project_email_addresse
     - does not exit, so that diffs can be emailed and permissions updated
     """
     log.debug('starting run_followup_tests()')
-    run_tests_initial_path = project_path / 'run_tests.py'
-    run_tests_path = run_tests_initial_path.resolve()
+    ## set the venv -------------------------------------------------
+    venv_tuple: tuple[Path, Path] = lib_common.determine_venv_paths(project_path)  # these are resolved-paths
+    (venv_bin_path, venv_path) = venv_tuple
+    local_scoped_env = os.environ.copy()
+    local_scoped_env['PATH'] = f'{venv_bin_path}:{local_scoped_env["PATH"]}'  # prioritizes venv-path
+    local_scoped_env['VIRTUAL_ENV'] = str(venv_path)
+    ## prep the command ---------------------------------------------
+    python_path = (
+        venv_bin_path / 'python3'
+    )  # don't resolve() -- venv_bin_path is already resolved; it'll use the system python-path and we want the venv python-path
+    log.debug(f'python_path, ``{python_path}``')
+    run_tests_path = project_path / 'run_tests.py'  # no need to resolve; project_path is already resolved
+    command = [str(python_path), str(run_tests_path)]
+    log.debug(f'followup runtests command: ``{command}``')
     try:
-        command = [str(uv_path), 'run', str(run_tests_path)]
-        log.debug(f'command: ``{command}``')
         subprocess.run(command, check=True)
         return_val = None
     except subprocess.CalledProcessError as e:
