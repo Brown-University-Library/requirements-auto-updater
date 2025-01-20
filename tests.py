3b9d8cc13538d8ce420ecac177122a5cb3770fea
Birkin James Diana <birkin.diana@gmail.com>
2025-01-20 12:39:59 -0500

    adds test for django-present-in-diff.

--- tests.py
+++ tests.py
@@ -27,10 +27,52 @@ log = logging.getLogger(__name__)
 this_file_path = Path(__file__).resolve()
 stuff_dir = this_file_path.parent.parent
 sys.path.append(str(stuff_dir))
+from self_updater_code import lib_django_updater  # noqa: E402  (prevents linter problem-indicator)
 from self_updater_code.lib_compilation_evaluator import CompiledComparator  # noqa: E402  (prevents linter problem-indicator)
 
 
-class TestSelfUpdater(unittest.TestCase):
+class TestMiscellaneous(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_check_for_django_update__no_django(self):
+        """
+        Check that django is not detected properly.
+        """
+        incoming_text = 'foo\nbar\nbaz\n'
+        expected = False
+        result = lib_django_updater.check_for_django_update(incoming_text)
+        self.assertEqual(expected, result)
+
+    def test_check_for_django_update__django_present(self):
+        """
+        Check that django is detected properly.
+        """
+        incoming_text = """
+            --- staging_2025-01-14T02-00-05.txt
+            +++ staging_2025-01-15T02-00-04.txt
+            ---
+            +++
+            @@ -12,7 +12,7 @@
+                #   httpx
+            cffi==1.17.1 ; implementation_name != 'pypy' and os_name == 'nt'
+                # via trio
+            -django==4.2.17
+            +django==4.2.18
+                # via -r requirements/base.in
+            h11==0.14.0
+                # via httpcore
+            """
+        incoming_text: str = incoming_text.replace('            ', '')  # removes indentation-spaces
+        expected = True
+        result = lib_django_updater.check_for_django_update(incoming_text)
+        self.assertEqual(expected, result)
+
+
+class TestComparison(unittest.TestCase):
     def setUp(self):
         self.compiled_comparator = CompiledComparator()
         pass
