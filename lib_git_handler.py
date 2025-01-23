cb393a030bd657581ae55ff010f6d2abeebbfcf5
Birkin James Diana <birkin.diana@gmail.com>
2025-01-23 08:19:48 -0500

    updates docstring.

--- lib_git_handler.py
+++ lib_git_handler.py
@@ -7,6 +7,8 @@ log = logging.getLogger(__name__)
 
 def run_git_pull() -> tuple[bool, dict]:
     """
+    (not yet used by code -- just by a test)
+
     Runs `git pull` and return the output similar to Go's (ok, err) format.
     Possible TODO: pass in the dir-path as an argument.
     Note to self: subprocess.run's `cwd` param changes the current-working-directory before the command is run,
