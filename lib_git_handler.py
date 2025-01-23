059f6bdff0d6dabd36f9aaa416d90efa311dae04
Birkin James Diana <birkin.diana@gmail.com>
2025-01-23 08:04:29 -0500

    implements git-pull caller.

--- lib_git_handler.py
+++ lib_git_handler.py
@@ -5,15 +5,20 @@ from pathlib import Path
 log = logging.getLogger(__name__)
 
 
-def run_git_pull() -> str:
+def run_git_pull() -> tuple[bool, dict]:
     """
-    Run `git pull` and return the output.
+    Runs `git pull` and return the output similar to Go's (ok, err) format.
+    Possible TODO: pass in the dir-path as an argument.
+    Note to self: subprocess.run's `cwd` param changes the current-working-directory before the command is run,
+      and leaves it there.
     """
     cur_dir = Path('./').resolve()
     log.debug(f'cur_dir: {cur_dir}')
     command = ['git', 'pull']
-    # result = subprocess.run(command, cwd=cur_dir, check=True, capture_output=True, text=True)
-    result = subprocess.run(command, cwd=cur_dir, capture_output=True, text=True)
-    log.debug(f'type(result): {type(result)}')
+    result: subprocess.CompletedProcess = subprocess.run(command, cwd=cur_dir, capture_output=True, text=True)
     log.debug(f'result: {result}')
-    return result
+    ok = True if result.returncode == 0 else False
+    output = {'stdout': f'{result.stdout}', 'stderr': f'{result.stderr}'}
+    return_val = (ok, output)
+    log.debug(f'return_val: {return_val}')
+    return return_val
