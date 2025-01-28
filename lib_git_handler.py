720868add417520a6fc6a116022bf328471c4a4e
Birkin James Diana <birkin.diana@gmail.com>
2025-01-28 07:55:59 -0500

    adds git-commit and git-push handlers.

--- lib_git_handler.py
+++ lib_git_handler.py
@@ -54,3 +54,45 @@ def run_git_add(requirements_path: Path, project_path: Path) -> tuple[bool, dict
     return_val = (ok, output)
     log.debug(f'return_val: {return_val}')
     return return_val
+
+
+def run_git_commit(project_path: Path, commit_message: str | None = None) -> tuple[bool, dict]:
+    """
+    Runs `git commit` and return the output.
+    """
+    log.info('::: running git commit ----------')
+    if commit_message is None:
+        commit_message = 'auto-update of requirements'
+    command = ['git', 'commit', '-m', commit_message]
+    result: subprocess.CompletedProcess = subprocess.run(command, cwd=str(project_path), capture_output=True, text=True)
+    log.debug(f'result: {result}')
+    ok = True if result.returncode == 0 else False
+    if ok is True:
+        log.info('ok / git commit successful')
+    else:
+        if 'nothing to commit' in result.stdout:
+            log.info('ok / nothing to commit')
+    output = {'stdout': f'{result.stdout}', 'stderr': f'{result.stderr}'}
+    return_val = (ok, output)
+    log.debug(f'return_val: {return_val}')
+    return return_val
+
+
+def run_git_push(project_path: Path) -> tuple[bool, dict]:
+    """
+    Runs `git push` and return the output.
+    """
+    log.info('::: running git push ----------')
+    command = ['git', 'push', 'origin', 'main']
+    result: subprocess.CompletedProcess = subprocess.run(command, cwd=str(project_path), capture_output=True, text=True)
+    log.debug(f'result: {result}')
+    ok = True if result.returncode == 0 else False
+    if ok is True:
+        if 'Everything up-to-date' in result.stderr:
+            log.info('ok / git push showed "Everything up-to-date"')
+        else:
+            log.info('ok / git push successful')
+    output = {'stdout': f'{result.stdout}', 'stderr': f'{result.stderr}'}
+    return_val = (ok, output)
+    log.debug(f'return_val: {return_val}')
+    return return_val
