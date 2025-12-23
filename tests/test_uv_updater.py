7002a092485d86b7422cfcf552afee2a1e0f38c3
Birkin James Diana <birkin.diana@gmail.com>
2025-12-23 09:54:14 -0500

    removes commented-out test.

--- tests/test_uv_updater.py
+++ tests/test_uv_updater.py
@@ -15,17 +15,6 @@ log = logging.getLogger(__name__)
 
 
 class TestUvUpdater(unittest.TestCase):
-    # def test_make_sync_command_includes_exclude_newer(self) -> None:
-    #     updater = UvUpdater()
-    #     uv_path = Path('/made/up/path')
-    #     sync_command = updater.make_sync_command(uv_path, 'local', 'foo')
-    #     self.assertIn('--exclude-newer', sync_command)
-    #     exclude_newer_index = sync_command.index('--exclude-newer')
-    #     self.assertLess(exclude_newer_index + 1, len(sync_command))
-    #     iso_date_str = sync_command[exclude_newer_index + 1]
-    #     datetime.datetime.strptime(iso_date_str, '%Y-%m-%d')  # ensures valid ISO date
-    #     self.assertEqual([str(uv_path), 'sync', 'foo', '--group', 'local'], sync_command[:5])
-
     def test_compare_uv_lock_files_happy_path_returns_diff(self) -> None:
         """
         Checks that compare_uv_lock_files() returns a dict indicating changes with unified diff text when files differ.
